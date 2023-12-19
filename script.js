f2815020b621ed74218175db67219ff848eb1267
vinayakmishra19 <vinayakmishra1978@gmail.com>
2023-12-20 00:46:06 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const apikey ="5a8d0b7c9762a1c71521416a50fed074";
+
+const apiurl ="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchbox = document.querySelector(".search input");
+const searchbtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+  const response = await fetch(apiurl + city + `&appid=${apikey}`);
+
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather-info").style.display = "none";
+  } else {
+    var data = await response.json();
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temperature").innerHTML =Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+    if (data.weather[0].main == "Clouds")
+    {
+      weatherIcon.src = "images/clouds.png";
+    } 
+    else if (data.weather[0].main == "Clear") 
+    {
+      weatherIcon.src = "images/clear.png";
+    } 
+    else if (data.weather[0].main == "Rain") 
+    {
+      weatherIcon.src = "images/rain.png";
+    }
+
+    document.querySelector(".weather-info").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  }
+}
+
+searchbtn.addEventListener("click", () => {
+  checkWeather(searchbox.value);
+});
