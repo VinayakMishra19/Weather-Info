3576e7fe92084f8a9815982ce23aaaf03606b1ff
vinayakmishra19 <vinayakmishra1978@gmail.com>
2023-12-21 19:59:14 +0530

    NewAdditionalFeatures

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const apikey ="5a8d0b7c9762a1c71521416a50fed074";
 
 const apiurl ="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
@@ -9,21 +10,30 @@ const weatherIcon = document.querySelector(".weather-icon");
 
 
 
-const button= document.getElementById('Current-location');
+const getlocationicon= document.getElementById('Current-location');
 
-async function gotlocation(position){
+
+/*async function gotlocation(position){
     getdata(position.coords.latitude, position.coords.longitude)
 }
 
 function failedtoget(){
   console.log('There are some issues');
 }
-button.addEventListener('click', async () =>{
-     navigator.geolocation.getCurrentPosition(gotlocation, failedtoget);
+*/
+
+// User Ip address for getting his/her location without any typping work.....
+getlocationicon.addEventListener('click', async () =>{
+    const result= await fetch("https://ipapi.co/json/");
+
+    var data=await result.json();
+    // console.log(data);
+    getdata(data.city);
 })
 
-async function getdata(lat, lon ) {
-  const responselatilongi = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${lat}&lon=${lon}` + `&appid=${apikey}`);
+// Will directly get location access without any input
+async function getdata(city) {
+  const responselatilongi = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}` + `&appid=${apikey}`);
 
   if (responselatilongi.status == 404) 
   {
@@ -63,7 +73,7 @@ async function getdata(lat, lon ) {
 
 
 
-
+// this function will execute after getting the users input 
 async function checkWeather(city) {
   const response = await fetch(apiurl + city + `&appid=${apikey}`);
 
@@ -102,22 +112,66 @@ async function checkWeather(city) {
   }
 }
 
+// here will send the users city request into our main function so that api start to fetch the data for that city
 searchbtn.addEventListener("click", () => {
   checkWeather(searchbox.value);
 });
 
 
 
-
+//Dark-Light Theme....
 var icon = document.getElementById("Icon");
+var iconcurrentloc = document.getElementById("Current-location");
 icon.onclick = function () {
         document.body.classList.toggle("dark-theme");
         if(document.body.classList.contains("dark-theme")){
           icon.src="images/sun.png"
+          iconcurrentloc.src = "images/darkmode-currentlocation-icon.png"
         }
         else{
           icon.src="images/moon.png"
+          iconcurrentloc.src = "images/lightmode-currentlocation-icon.png"
         }
 }
 
 
+// Debouncing in query 
+let counter = 0;
+function getData(){
+       console.log("Fetching... Data "+ counter++);
+}
+
+function Debouncing(call,d){
+         let timer;
+         return function(...args){
+               if(timer) 
+               {
+                 clearTimeout(timer)
+               }
+              timer= setTimeout(()=>{
+                   call();
+               },d)
+         }
+
+}
+
+const goodFunction=Debouncing(getData,2000);
+
+
+// Throttle in searching button
+
+const throttle = (fun,d)=>{
+  
+  
+  return function(...args){
+    document.getElementById("Searchicon").disabled=true;
+    setTimeout(()=>{
+       fun();
+    },d)
+}
+}
+
+const Funcallinfo= throttle(()=>{
+      document.getElementById("Searchicon").disabled=false;
+      console.log("User Requested for some query ");
+},1000)
